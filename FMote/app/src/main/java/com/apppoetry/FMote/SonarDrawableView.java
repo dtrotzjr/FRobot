c48ec3480fae21bbfe914c9b15d88cdaf79e0c9b
David Trotz <davidtrotz@olivetree.com>
2014-01-01 22:15:02 -0800

    Some more work on the Android app.

--- FMote/app/src/main/java/com/apppoetry/FMote/SonarDrawableView.java
+++ FMote/app/src/main/java/com/apppoetry/FMote/SonarDrawableView.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.ArcShape;
 import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.RectShape;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -20,6 +21,8 @@ public class SonarDrawableView extends View {
     private AngleValuePair mAngleValuePairs[];
     private int mAngleValuePairLength;
     private final int MAX_ANGLE_VALUE_PAIRS = 32;
+    private int mMaxValue;
+    private int mMinValue;
 
     private class AngleValuePair {
         private int mAngle;
@@ -40,18 +43,11 @@ public class SonarDrawableView extends View {
     }
 
     private void init() {
-        int x = 10;
-        int y = 10;
-        int width = 300;
-        int height = 50;
-
-//        mDrawable = new ShapeDrawable(new OvalShape());
-//        mDrawable.getPaint().setColor(0xff74AC23);
-//        mDrawable.setBounds(x, y, x + width, y + height);
-
         mAngleValuePairs = null;
         mAngleValuePairLength = 0;
         mSonarResultMessage = "";
+        mMaxValue = 0;
+        mMinValue = 99999;
     }
 
     public SonarDrawableView(Context context) {
@@ -94,7 +90,13 @@ public class SonarDrawableView extends View {
                 if (subStartIndex >= 0 && subEndIndex > 0) {
                     StringTokenizer pairTokenizer = new StringTokenizer(angleValuePair.substring(subStartIndex + 1, subEndIndex - 1), ",");
                     if(pairTokenizer.countTokens() == 2) {
-                        mAngleValuePairs[pairIndex++] = new AngleValuePair(pairTokenizer.nextToken(), pairTokenizer.nextToken());
+                        mAngleValuePairs[pairIndex] = new AngleValuePair(pairTokenizer.nextToken(), pairTokenizer.nextToken());
+                        int curValue = mAngleValuePairs[pairIndex].getValue();
+                        if (curValue > mMaxValue)
+                            mMaxValue = curValue;
+                        if (curValue < mMinValue)
+                            mMinValue = curValue;
+                        pairIndex++;
                     }
                 }
             }
@@ -102,18 +104,67 @@ public class SonarDrawableView extends View {
         mAngleValuePairLength = pairIndex;
     }
 
+    protected void drawTruck(Canvas canvas)
+    {
+        int margin = 15;
+        int maxDrawArea = getMeasuredWidth() < getMeasuredHeight() ? getMeasuredWidth() : getMeasuredHeight();
+        ShapeDrawable truckBody = new ShapeDrawable(new RectShape());
+        int truckLength = (int)((maxDrawArea / 2.0) - margin);
+        int truckWidth = (int)((maxDrawArea / 6.0));
+        int top = (int)((maxDrawArea - truckWidth) / 2.0);
+        truckBody.setBounds(margin, top, margin + truckLength, top + truckWidth);
+        truckBody.getPaint().setARGB(196,196,16,32);
+        truckBody.draw(canvas);
+
+        int wheelLen = 40;
+        int wheelWidth = 20;
+        ShapeDrawable truckWheel1 = new ShapeDrawable(new RectShape());
+        top -= wheelWidth + 5;
+        int left = margin;
+        truckWheel1.setBounds(left, top, left + wheelLen, top + wheelWidth);
+        truckWheel1.draw(canvas);
+
+        ShapeDrawable truckWheel2 = new ShapeDrawable(new RectShape());
+        left = margin + truckLength - wheelLen;
+        truckWheel2.setBounds(left, top, left + wheelLen, top + wheelWidth);
+        truckWheel2.draw(canvas);
+
+        ShapeDrawable truckWheel3 = new ShapeDrawable(new RectShape());
+        top += wheelWidth + 5 + truckWidth + 5;
+        left = margin;
+        truckWheel3.setBounds(left, top, left + wheelLen, top + wheelWidth);
+        truckWheel3.draw(canvas);
+
+        ShapeDrawable truckWheel4 = new ShapeDrawable(new RectShape());
+        left = margin + truckLength - wheelLen;
+        truckWheel4.setBounds(left, top, left + wheelLen, top + wheelWidth);
+        truckWheel4.draw(canvas);
+    }
     protected void onDraw(Canvas canvas) {
         parseResultMessage();
 
+        int maxArcRadius = getMeasuredWidth() < getMeasuredHeight() ? getMeasuredWidth() : getMeasuredHeight();
+        float scaleFactor = ((float)maxArcRadius/(float)mMaxValue);
+
+
         for (int pairIndex = 0; pairIndex < mAngleValuePairLength; pairIndex++) {
             AngleValuePair pair = mAngleValuePairs[pairIndex];
-            ShapeDrawable drawable = new ShapeDrawable(new ArcShape(pair.getAngle() - 7, 14));
-            drawable.setAlpha(128);
-            drawable.getPaint().setColor(0x7F74AC23);
-            drawable.setBounds(10, 10, getMeasuredWidth() - 20, getMeasuredHeight() - 20);
+            ShapeDrawable drawable = new ShapeDrawable(new ArcShape(-pair.getAngle() - 7, 14));
+            int value = pair.getValue();
+            // Set the alpha based on how close the value is to the max
+            int alphaValue = (int)(196.0*((float)value/(float)mMaxValue));
+            drawable.getPaint().setColor(0xFF74AC23);
+            drawable.getPaint().setAlpha(alphaValue);
+
+            // Set the radius based on how close the value is to the max value
+            int arcRadius = (int)(value * scaleFactor);
+            if (arcRadius % 2 != 0)
+                arcRadius--; // make it a power of two so that the arcs line up.
+            int margin = (int)((double)(maxArcRadius - arcRadius) / 2.0);
+            drawable.setBounds(margin, margin, margin + arcRadius, margin + arcRadius);
             drawable.draw(canvas);
         }
 
-
+        drawTruck(canvas);
     }
 }
